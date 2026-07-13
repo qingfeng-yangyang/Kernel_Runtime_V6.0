88dc4439ae3096cd6714e5b3bd8af9ae042fb3c6
qingfeng-yangyang <yangqingming95@gmail.com>
2026-07-13 23:16:18 +0800

    Delete src/applications/ecommerce/V5_modules directory

--- src/applications/ecommerce/V5_modules/__init__.py
+++
@@ -1,7 +0,0 @@
-from .customer import CustomerAgent
-from .dispatcher import DispatcherAgent
-from .quality import QualityAgent
-from .worker import WorkerModule
-from .writer import WriterAgent
-
-__all__ = ["DispatcherAgent", "CustomerAgent", "WorkerModule", "WriterAgent", "QualityAgent"]
